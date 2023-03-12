4e0b7b25428d54b77ca798914f4241c9f11c6fbf
Rotbjorn <rotbjorn@outlook.com>
2023-03-12 18:14:12 +0100

    Parse program and statements + added ParseResult

--- cas_lib/src/cas/parser.rs
+++ cas_lib/src/cas/parser.rs
@@ -1,9 +1,13 @@
 use core::panic;
+use crate::cas::error::ParseError;
 
 use super::{token::{Token, TokenType}, node::{Node, OperationType}};
 
+type ParseResult<T> = std::result::Result<T, ParseError>;
+
 pub struct Parser {
     tokens: Vec<Token>,
+    // TODO: Reference from self.tokens instead?
     cur_token: Token,
     idx: usize,
 }
@@ -18,22 +22,91 @@ impl Parser {
         }
     }
 
-    pub fn parse(&mut self) -> Node {
-        self.parse_expression()
+    pub fn parse(&mut self) -> ParseResult<Node> {
+        let mut nodes: Vec<Node> = Vec::new();
+
+        loop {
+            let result = self.parse_statement();
+            match result {
+                Ok(node) => nodes.push(node),
+                Err(err) => {
+                    match &err {
+                        ParseError::EndOfStream => {
+                            break
+                        },
+                        ParseError::WrongToken { message: _ } => return Err(err)
+
+                    }
+                }
+            }
+        }
+        return Ok(Node::Program(nodes))
     }
+
+
 }
 
 impl Parser {
-    fn parse_expression(&mut self) -> Node {
+    fn parse_statement(&mut self) -> ParseResult<Node> {
+        match self.cur_token.typ {
+            TokenType::Identifier(_) => return self.parse_function_possible(),
+            _ => return self.parse_expression(),
+        }
+    }
+
+    fn parse_function_possible(&mut self) -> ParseResult<Node> {
+        let TokenType::Identifier(name) = self.cur_token.typ.clone() else {
+            // FIX?
+            self.expect(TokenType::Caret, "message")?;
+            panic!("Smack");
+        };
+        self.consume()?;
+
+        if self.cur_token.typ == TokenType::LeftParenthesis {
+            // Function!
+            // parse parameters
+            self.consume()?;
+
+            self.parse_parameter_and_type()?;
+
+            let _ = self.expect(TokenType::RightParenthesis, "Expected closing parenthesis after parameter list!");
+            let _ = self.expect(TokenType::Equal, "Expected assignment operator after function definition...");
+        }
+
+        let function_body = self.parse_expression()?; 
+
+        Ok(Node::Function {
+            name, 
+            function_body: Box::new(function_body)
+        })
+    }
+
+    fn parse_parameter_and_type(&mut self) -> ParseResult<(String, Token)> {
+        let TokenType::Identifier(param_name) = self.cur_token.typ.clone() else {
+            panic!("Oh no!");
+        };
+        self.consume()?;
+        // TODO: Do not use expect? => Instead check if type annotation is present to start with.
+        self.expect(TokenType::Colon, "Expected colon after parameter name.")?;
+        
+        let Ok(type_name) = self.expect_identifier("Expected type name after semicolon.") else {
+            panic!("No type name stuff");
+        };
+
+        Ok((param_name, type_name))
+    }
+
+    fn parse_expression(&mut self) -> ParseResult<Node> {
         self.parse_precedence(Precedence::None)
     }
 
-    fn parse_precedence(&mut self, prec: Precedence) -> Node {
+    fn parse_precedence(&mut self, prec: Precedence) -> ParseResult<Node> {
         let mut node = match self.cur_token.typ {
-            TokenType::Number(_) => self.parse_number(),
-            TokenType::Identifier(_) => self.parse_identifier(),
+            TokenType::Number(_) => self.parse_number()?,
+            TokenType::Identifier(_) => self.parse_identifier()?,
+            TokenType::Keyword(_) => self.parse_keyword(),
             TokenType::Minus => todo!(),
-            TokenType::LeftParenthesis => self.parse_grouping(),
+            TokenType::LeftParenthesis => self.parse_grouping()?,
             TokenType::LeftSquareBracket => todo!(),
             TokenType::RightSquareBracket => todo!(),
             TokenType::RightBrace => todo!(),
@@ -48,11 +121,11 @@ impl Parser {
 
         while self.idx < self.tokens.len() && prec <= token_precedence(&self.cur_token.typ) {
             node = match self.cur_token.typ {
-                TokenType::Plus => self.parse_addition(node),
-                TokenType::Minus => self.parse_subtraction(node),
-                TokenType::Star => self.parse_multiplication(node),
-                TokenType::Slash => self.parse_division(node),
-                TokenType::Caret => self.parse_power(node),
+                TokenType::Plus => self.parse_addition(node)?,
+                TokenType::Minus => self.parse_subtraction(node)?,
+                TokenType::Star => self.parse_multiplication(node)?,
+                TokenType::Slash => self.parse_division(node)?,
+                TokenType::Caret => self.parse_power(node)?,
                 _ => {
                     break;
                 }
@@ -61,48 +134,52 @@ impl Parser {
         }
 
         //println!("\nReturning node: {:?}\n", node);
-        node
+        Ok(node)
     }
 
-    fn parse_number(&mut self) -> Node {
+    fn parse_number(&mut self) -> ParseResult<Node> {
         let TokenType::Number(n) = self.cur_token.typ else {
             panic!("shit");
         };
 
-        self.consume();
-        return Node::Number(n)
+        self.consume()?;
+        return Ok(Node::Number(n))
     }
 
-    fn parse_identifier(&mut self) -> Node {
+    fn parse_identifier(&mut self) -> ParseResult<Node> {
         let TokenType::Identifier(s) = self.cur_token.typ.clone() else {
             panic!("Oh no.");
         };
-        self.consume();
-        return Node::Variable(s);
+        self.consume()?;
+        return Ok(Node::Variable(s))
     }
 
-    fn parse_grouping(&mut self) -> Node {
-        self.consume();
-        let node = self.parse_expression();
-        self.expect(TokenType::RightParenthesis);
-
-        node
+    fn parse_keyword(&self) -> Node {
+        todo!()
     }
 
-    fn parse_addition(&mut self, left: Node) -> Node {
-        self.consume();
-        let right = self.parse_precedence(Precedence::Factor);
+    fn parse_grouping(&mut self) -> ParseResult<Node> {
+        self.consume()?;
+        let node = self.parse_expression()?;
+        self.expect(TokenType::RightParenthesis, "Expected closing parenthesis")?;
+
+        Ok(node)
+    }
+
+    fn parse_addition(&mut self, left: Node) -> ParseResult<Node> {
+        self.consume()?;
+        let right = self.parse_precedence(Precedence::Factor)?;
         let node = Node::BinaryOp {
             left: left.into(),
             operation: OperationType::Add,
             right: right.into(),
         };
-        node
+        Ok(node)
     }
 
-    fn parse_subtraction(&mut self, left: Node) -> Node {
-        self.consume();
-        let right = self.parse_precedence(Precedence::Factor);
+    fn parse_subtraction(&mut self, left: Node) -> ParseResult<Node> {
+        self.consume()?;
+        let right = self.parse_precedence(Precedence::Factor)?;
         let right_node = Node::BinaryOp {
             left: (Node::Number(-1.0).into()),
             operation: OperationType::Multiply,
@@ -114,23 +191,23 @@ impl Parser {
             operation: OperationType::Add,
             right: right_node.into(),
         };
-        node
+        Ok(node)
     }
 
-    fn parse_multiplication(&mut self, left: Node) -> Node {
-        self.consume();
-        let right = self.parse_precedence(Precedence::Exponent);
+    fn parse_multiplication(&mut self, left: Node) -> ParseResult<Node> {
+        self.consume()?;
+        let right = self.parse_precedence(Precedence::Exponent)?;
         let node = Node::BinaryOp {
             left: left.into(),
             operation: OperationType::Multiply,
             right: right.into(),
         };
-        node
+        Ok(node)
     }
 
-    fn parse_division(&mut self, left: Node) -> Node {
-        self.consume();
-        let right = self.parse_precedence(Precedence::Factor);
+    fn parse_division(&mut self, left: Node) -> ParseResult<Node> {
+        self.consume()?;
+        let right = self.parse_precedence(Precedence::Factor)?;
         let right_node = Node::BinaryOp {
             left: right.into(),
             operation: OperationType::Power,
@@ -142,40 +219,54 @@ impl Parser {
             operation: OperationType::Multiply,
             right: right_node.into(),
         };
-        node
+        Ok(node)
     }
 
-    fn parse_power(&mut self, left: Node) -> Node {
+    fn parse_power(&mut self, left: Node) -> ParseResult<Node> {
         // TODO: Something higher than Exponent?
-        self.consume();
-        let right = self.parse_precedence(Precedence::Exponent);
+        self.consume()?;
+        let right = self.parse_precedence(Precedence::Exponent)?;
         let node = Node::BinaryOp {
             left: left.into(),
             operation: OperationType::Power,
             right: right.into(),
         };
-        node
+        Ok(node)
     }
 
-    // TODO: Add message parameter for extra information
-    fn expect(&mut self, tok: TokenType) -> Result<(), ()> {
+    fn expect(&mut self, tok: TokenType, message: &str) -> ParseResult<Token> {
         if self.cur_token.typ != tok {
             // TODO: Change to ParseError?
-            panic!("Unexpected token!\n{:?}", self.cur_token.typ);
+            panic!("Unexpected token!\n{:?}\n{}", self.cur_token.typ, message);
         } else {
             self.consume()
         }
     }
+    fn expect_identifier(&mut self, message: &str) -> ParseResult<Token> {
+        let TokenType::Identifier(_) = self.cur_token.typ.clone() else {
+            return Err(ParseError::WrongToken  {
+                message: format!("Expected an identifier: {:?}\n{}", self.cur_token.typ, message)
+            });
+        };
+        let token = self.cur_token.clone();
+        self.consume()?;
+        Ok(token)
+    }
 
     // TODO: Return if end of tokenstream
-    fn consume(&mut self) -> Result<(), ()> {
-        self.idx += 1;
+    fn consume(&mut self) -> ParseResult<Token> {
+        let token = self.cur_token.clone();
         if self.idx < self.tokens.len() {
+            self.idx += 1;
             println!("Consuming token: {:?}!", self.cur_token);
-            self.cur_token = self.tokens[self.idx].clone();
-            Ok(())
+            // TODO: Change to something iterator-like to not have to do this???
+            if self.idx < self.tokens.len() {
+                self.cur_token = self.tokens[self.idx].clone();
+            }
+            Ok(token)
         } else {
-            Err(())
+            println!("We last consumed: {:?}", token);
+            return Err(ParseError::EndOfStream);
         }
     }
 }
