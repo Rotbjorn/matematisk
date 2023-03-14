fb4148c626595f61e9806c98993b24ceda8803b5
Rotbjorn <rotbjorn@outlook.com>
2023-03-14 12:54:40 +0100

    Impl Display for Position

--- cas_lib/src/util.rs
+++ cas_lib/src/util.rs
@@ -1,5 +1,13 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy)]
 pub struct Position {
     pub row: usize,
     pub col: usize,
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+       f.write_fmt(format_args!("(row {}, col {})", self.row, self.col)) 
+    }
 }
