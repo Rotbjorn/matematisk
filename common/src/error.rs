351cd1144c74b92273bd85f541d9534daa52bdfc
Rotbjorn <rotbjorn@outlook.com>
2023-03-25 18:06:25 +0100

    Added Display impl for ParseError

--- common/src/error.rs
+++ common/src/error.rs
@@ -1,6 +1,17 @@
+use std::fmt::Display;
+
 // TODO: Contain positon where error occured?
 #[derive(Debug, Clone)]
 pub enum ParseError {
     WrongToken { message: String },
     EndOfStream,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::WrongToken { message } => f.write_str(message),
+            ParseError::EndOfStream => f.write_str("End of stream"),
+        }
+    }
 }
