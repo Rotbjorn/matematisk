853d1ecfd067a3db8f9648d42152de48fe75704e
Rotbjorn <rotbjorn@outlook.com>
2023-03-31 17:34:16 +0200

    Added more errors

--- common/src/error.rs
+++ common/src/error.rs
@@ -1,17 +1,48 @@
 use std::fmt::Display;
 
+use crate::token::{KeywordType, Token, TokenType};
+
 // TODO: Contain positon where error occured?
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub enum ParseError {
-    WrongToken { message: String },
+    WrongToken {
+        message: String,
+        expected: TokenType,
+        actual: Token,
+    },
+    WrongKeyword {
+        message: String,
+        expected: KeywordType,
+        actual: Token,
+    },
+    NotIdentifier {
+        message: String,
+        actual: Token,
+    },
+    NotComparison {
+        message: String,
+        actual: Token,
+    },
     EndOfStream,
 }
 
 impl Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use ParseError::*;
         match self {
-            ParseError::WrongToken { message } => f.write_str(message),
-            ParseError::EndOfStream => f.write_str("End of stream"),
+            WrongToken {
+                message,
+                expected: _,
+                actual: _,
+            }
+            | WrongKeyword {
+                message,
+                expected: _,
+                actual: _,
+            } => f.write_str(message),
+            NotIdentifier { message, actual: _ } => f.write_str(message),
+            NotComparison { message, actual: _ } => f.write_str(message),
+            EndOfStream => f.write_str("End of stream"),
         }
     }
 }
