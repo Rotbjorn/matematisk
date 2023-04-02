a9530c8aec3b4bedab1966190247c9974a52cb7f
Rotbjorn <rotbjorn@outlook.com>
2023-04-02 10:53:17 +0200

    Added new inspector widget
    - Shows functions and variables
    - Nicer UI

--- gui/src/app/app.rs
+++ gui/src/app/app.rs
@@ -1,5 +1,5 @@
 use eframe::App;
-use egui::{TextEdit, Ui, panel::Side};
+use egui::{panel::Side, FontId, Label, RichText, TextEdit, Ui};
 use matex_compiler::cas::{
     backend::{
         format::{NormalFormatter, ValueFormatter},
@@ -11,11 +11,11 @@ use matex_compiler::cas::{
 use super::widgets::inspector::Inspector;
 
 struct State {
-
+    pane: ActivePane,
 }
 
 pub struct MatexApp {
-    _state: State,
+    state: State,
     source: String,
 
     executed: Vec<(String, String)>,
@@ -25,8 +25,12 @@ pub struct MatexApp {
 impl MatexApp {
     fn render_executions(&self, ui: &mut Ui) {
         for (source, output) in &self.executed {
-            ui.label("i> ".to_owned() + source);
-            ui.label("o>".to_owned() + output);
+            ui.add(Label::new(
+                RichText::new("i>".to_owned() + source).font(FontId::monospace(12.0)),
+            ));
+            ui.add(Label::new(
+                RichText::new("o>".to_owned() + output).font(FontId::monospace(12.0)),
+            ));
             ui.separator();
         }
     }
@@ -35,12 +39,20 @@ impl MatexApp {
 impl Default for MatexApp {
     fn default() -> Self {
         Self {
-            _state: State {},
+            state: State {
+                pane: ActivePane::REPL,
+            },
             source: "".to_owned(),
             executed: Vec::new(),
             runtime: Runtime::new(),
         }
     }
+}
+
+#[derive(PartialEq)]
+enum ActivePane {
+    REPL,
+    Example,
 }
 
 impl App for MatexApp {
@@ -49,41 +61,66 @@ impl App for MatexApp {
             let scope = self.runtime.environment.get_scope();
             Inspector::ui(ui, &scope.functions, &scope.variables);
         });
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            egui::Window::new("REPL").show(ctx, |ui| {
-                self.render_executions(ui);
-                /*
-                let mut visuals = Visuals::default();
-                visuals.panel_fill = Color32::DARK_RED;
-                ctx.set_visuals(visuals);
-                */
-                TextEdit::multiline(&mut self.source).show(ui);
-
-                if ui.button("Run").clicked() {
-                    if let Ok(program) = Parser::new(Lexer::new(&self.source).collect()).parse() {
-                        let last_value = self.runtime.run(&program);
-                        self.executed.push((
-                            self.source.clone(),
-                            format!("{}", NormalFormatter::format(&last_value)),
-                        ));
-                        self.source.clear();
-                    }
-                }
-
-                /*
-                let sin: PlotPoints = (0..1000).map(|i| {
-                    let x = i as f64 * 0.1;
-                    [x, x.sin()]
-                }).collect();
-                let line = Line::new(sin);
-                let line = line.stroke((32.0, Color32::GOLD));
-
-
-                Plot::new("test-plot").show_background(false).show(ui, |plot_ui| {
-                    plot_ui.line(line)
-                });
-                */
+            ui.horizontal(|ui| {
+                ui.selectable_value(&mut self.state.pane, ActivePane::REPL,  RichText::new("REPL").heading());
+                ui.selectable_value(&mut self.state.pane, ActivePane::Example,  RichText::new("Example").heading());
             });
+
+            match self.state.pane {
+                ActivePane::REPL => {
+                    self.render_executions(ui);
+
+                    TextEdit::multiline(&mut self.source)
+                        .code_editor()
+                        .hint_text("Write some code")
+                        .show(ui);
+
+                    if ui.button("Run").clicked() {
+                        if let Ok(program) = Parser::new(Lexer::new(&self.source).collect()).parse() {
+                            let last_value = self.runtime.run(&program);
+                            self.executed.push((
+                                self.source.clone(),
+                                format!("{}", NormalFormatter::format(&last_value)),
+                            ));
+                            self.source.clear();
+                        }
+                    }
+                },
+                ActivePane::Example => {
+                    TextEdit::multiline(&mut EXAMPLE_CODE.to_owned())
+                        .font(egui::TextStyle::Monospace)
+                        .code_editor()
+                        .show(ui);
+
+
+                    ui.separator();
+                    ui.label(
+                        r"Finns just nu inte möjlighet att göra kommentarer
+Man får inte heller göra ett funktions anrop först (på en linje), för den tror att det är en funktionsdefinition
+e.g. add(1, 2). Är enkelt att fixa men har inte gjort det än, är annars bara att spara funktionsvärdet i en variabel
+så fungerar det: y = add(1, 2).
+Som visat ovanför så går det också att skriva in symboler i funktioner och få tillbaka ett uttryck.
+När jag lägger till funktioner som cos och sin, så måste sådana funktioner kunna uttryckas i uttryck, 
+te.x. cos(x) måste förvaras till värdet på x är definierat.
+Just nu gör typerna efter variablerna inget, men de är obligatoriska att ha med!
+
+Tänker att nästa steg är bättre förenkling + ekvationslösning samt reaktiva variabler");
+                },
+            }
         });
     }
 }
+
+const EXAMPLE_CODE: &'static str = r"add(x: r, y: hund) = x + y
+a = add(2, 3)
+b = add(c, d)
+
+y = l*f - l*2*f
+simplify y
+i = simplify y
+i
+
+h = simplify add(12*x, 13*x)
+";
--- gui/src/app/widgets/inspector.rs
+++ gui/src/app/widgets/inspector.rs
@@ -1,23 +1,54 @@
-use egui::Ui;
-use matex_common::{util::SymbolTable, function::Function};
-use matex_compiler::cas::backend::{format::{NormalFormatter, ValueFormatter}, value::RuntimeVal};
+use egui::{RichText, Ui};
+use matex_common::{
+    function::{Function, Parameter},
+    util::SymbolTable,
+};
+use matex_compiler::cas::backend::{
+    format::{NormalFormatter, ValueFormatter},
+    value::RuntimeVal,
+};
 
-pub struct Inspector {
-
-}
+pub struct Inspector {}
 
 impl Inspector {
     pub fn ui(ui: &mut Ui, functions: &SymbolTable<Function>, variables: &SymbolTable<RuntimeVal>) {
-        ui.collapsing("Functions", |ui| {
-            for (key, value) in functions {
-                ui.label(format!("{}: {:?}", key, value));
-            }
+        ui.collapsing(RichText::new("Functions").heading(), |ui| {
+            egui::Grid::new("func_grid")
+                .num_columns(1)
+                .spacing(egui::vec2(0.0, 4.0))
+                .striped(true)
+                .show(ui, |ui| {
+                    for (_, value) in functions {
+                        let Function { name, params, body } = value;
+                        let params_string = params
+                            .iter()
+                            .map(|param| param.name.clone())
+                            .collect::<Vec<_>>()
+                            .join(", ");
+                        let function_string = format!("{}({})", name, params_string);
+                        ui.collapsing(function_string, |ui| {
+                            for Parameter { name, type_name } in params {
+                                ui.label(format!("{name}: {type_name}"));
+                            }
+                            ui.label(format!("body: {:?}", body));
+                        });
+                        ui.end_row();
+                    }
+                });
         });
         ui.separator();
-        ui.collapsing("Variables", |ui| {
-            for (key, value) in variables {
-                ui.label(format!("{}: {}", key, NormalFormatter::format(value)));
-            }
+        ui.collapsing(RichText::new("Variables").heading(), |ui| {
+            egui::Grid::new("var_grid")
+                .num_columns(2)
+                .spacing(egui::vec2(10.0, 4.0))
+                .striped(true)
+                .show(ui, |ui| {
+                    for (name, value) in variables {
+                        ui.label(name);
+                        ui.label(NormalFormatter::format(value));
+                        ui.end_row();
+                    }
+                });
         });
     }
 }
